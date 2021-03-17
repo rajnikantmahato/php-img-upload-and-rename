5617b1ce33b0adc2849948d1e103e91031c4d437
rajnikantmahato <58804492+rajnikantmahato@users.noreply.github.com>
2021-03-17 12:42:07 +0000

    Create up.php

---
+++ up.php
@@ -0,0 +1,46 @@
+<?php
+
+// upload and Rename File
+
+if (isset($_POST['submit']))
+{
+	$filename = $_FILES["file"]["name"];
+	$file_basename = substr($filename, 0, strripos($filename, '.')); // get file extention
+	$file_ext = substr($filename, strripos($filename, '.')); // get file name
+	$filesize = $_FILES["file"]["size"];
+	$allowed_file_types = array('.gif','.png','.jpg','.jpeg');	
+
+	if (in_array($file_ext,$allowed_file_types) && ($filesize < 200000))
+	{	
+		// Rename file
+		$newfilename = md5($file_basename) . $file_ext;
+		if (file_exists("uploads/" . $newfilename))
+		{
+			// file already exists error
+			echo "You have already uploaded this file.";
+		}
+		else
+		{		
+			move_uploaded_file($_FILES["file"]["tmp_name"], "uploads/" . $newfilename);
+			echo "File uploaded successfully.";		
+		}
+	}
+	elseif (empty($file_basename))
+	{	
+		// file selection error
+		echo "Please select a file to upload.";
+	} 
+	elseif ($filesize > 200000)
+	{	
+		// file size error
+		echo "The file you are trying to upload is too large.";
+	}
+	else
+	{
+		// file type error
+		echo "Only these file typs are allowed for upload: " . implode(', ',$allowed_file_types);
+		unlink($_FILES["file"]["tmp_name"]);
+	}
+}
+
+?>
